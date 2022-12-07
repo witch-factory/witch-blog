6300907caf1a5482a7bddd988fd9a77d7c39764d
witch-factory <soakdma37@gmail.com>
2022-12-07 23:04:44 +0900

    썸네일 변경

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,33 +1,43 @@
-import { Link } from 'gatsby';
-import { StaticImage } from 'gatsby-plugin-image';
-import React, { memo, PropsWithChildren } from 'react';
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import React, { memo, PropsWithChildren } from "react";
 
-import { useAuthorProfile } from '~/hooks/useAuthorProfile';
-import { useCheckAboutPage } from '~/hooks/useCheckAboutPage';
+import { useAuthorProfile } from "~/hooks/useAuthorProfile";
+import { useCheckAboutPage } from "~/hooks/useCheckAboutPage";
 
-import { Container, ExternalLinks, LinkItem, Name, Description, Wrapper } from './styles';
-
+import {
+  Container,
+  ExternalLinks,
+  LinkItem,
+  Name,
+  Description,
+  Wrapper,
+} from "./styles";
 
 interface SocialLinkProps {
-  username?: string,
-  urlPrefix: string,
+  username?: string;
+  urlPrefix: string;
 }
 
-const ExternalLink = ({ username, urlPrefix, children }: PropsWithChildren<SocialLinkProps>) => {
+const ExternalLink = ({
+  username,
+  urlPrefix,
+  children,
+}: PropsWithChildren<SocialLinkProps>) => {
   if (!username) {
     return null;
   }
 
   return (
     <LinkItem>
-      <a href={`${urlPrefix}${username}`}>{ children }</a>
+      <a href={`${urlPrefix}${username}`}>{children}</a>
     </LinkItem>
   );
 };
 
 interface SocialLink {
-  text: string,
-  url: string,
+  text: string;
+  url: string;
 }
 
 const Profile = () => {
@@ -41,38 +51,38 @@ const Profile = () => {
 
   const socialLinks: Record<keyof GatsbyTypes.Social, SocialLink> = {
     github: {
-      text: 'GitHub',
-      url: 'https://github.com/',
+      text: "GitHub",
+      url: "https://github.com/",
     },
     twitter: {
-      text: 'Twitter',
-      url: 'https://twitter.com/',
+      text: "Twitter",
+      url: "https://twitter.com/",
     },
     facebook: {
-      text: 'Facebook',
-      url: 'https://www.facebook.com/',
+      text: "Facebook",
+      url: "https://www.facebook.com/",
     },
     instagram: {
-      text: 'Instagram',
-      url: 'https://www.instagram.com/',
+      text: "Instagram",
+      url: "https://www.instagram.com/",
     },
     linkedin: {
-      text: 'LinkedIn',
-      url: 'https://www.linkedin.com/in/',
-    }
+      text: "LinkedIn",
+      url: "https://www.linkedin.com/in/",
+    },
   };
 
   return (
     <Container>
       <StaticImage
-        className='profile-image'
-        layout='fixed'
-        formats={['auto', 'webp', 'avif']}
-        src='../../images/witch.jpg'
+        className="profile-image"
+        layout="fixed"
+        formats={["auto", "webp", "avif"]}
+        src="../../images/witch2.jpeg"
         width={70}
         height={70}
         quality={95}
-        alt='Profile picture'
+        alt="Profile picture"
       />
       <Wrapper>
         <Name>
@@ -82,16 +92,20 @@ const Profile = () => {
         <Description>{description}</Description>
 
         <ExternalLinks>
-          {isAboutPageExists &&
-              <LinkItem>
-                <Link to={'/about'}>About</Link>
-              </LinkItem>
-          }
+          {isAboutPageExists && (
+            <LinkItem>
+              <Link to={"/about"}>About</Link>
+            </LinkItem>
+          )}
           {Object.entries(social ?? {}).map(([key, username]) => {
             const serviceName = key as keyof GatsbyTypes.Social;
 
             return (
-              <ExternalLink key={serviceName} username={username} urlPrefix={socialLinks[serviceName].url}>
+              <ExternalLink
+                key={serviceName}
+                username={username}
+                urlPrefix={socialLinks[serviceName].url}
+              >
                 {socialLinks[serviceName].text}
               </ExternalLink>
             );
