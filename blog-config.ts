023ea74afdef8fcf5fd68dca6b5f4a7f7fd7087d
witch-factory <soakdma37@gmail.com>
2023-05-05 02:30:10 +0900

    블로그 자기소개 졸업 수정

--- blog-config.ts
+++ blog-config.ts
@@ -1,28 +1,28 @@
 const config = {
-  title: "Witch-Work", // Blog name
-  author: "김성현(Sung Hyun Kim)", // Your name
+  title: 'Witch-Work', // Blog name
+  author: '김성현(Sung Hyun Kim)', // Your name
   description:
-    "서강대학교에서 기계공학과 컴퓨터공학을 복수전공하고 있습니다. " +
-    "흔히 `마녀`라는 닉네임을 씁니다. " +
-    "어떤 대단한 뜻을 품고 사는 사람은 아닙니다. " +
-    "그저 멋진 사람들이 내는 빛을 따라가다 보니 여기까지 왔고, " +
-    "앞으로도 그렇게 살 수 있었으면 좋겠다고 생각하는 사람입니다. " +
-    "이곳에 찾아오신 당신과도 함께할 수 있어 영광입니다.",
-  siteUrl: "https://witch-work.vercel.app",
+    '서강대학교에서 기계공학과 컴퓨터공학을 복수전공하였습니다. ' +
+    '흔히 `마녀`라는 닉네임을 씁니다. ' +
+    '어떤 대단한 뜻을 품고 사는 사람은 아닙니다. ' +
+    '그저 멋진 사람들이 내는 빛을 따라가다 보니 여기까지 왔고, ' +
+    '앞으로도 그렇게 살 수 있었으면 좋겠다고 생각하는 사람입니다. ' +
+    '이곳에 찾아오신 당신과도 함께할 수 있어 영광입니다.',
+  siteUrl: 'https://witch-work.vercel.app',
   social: {
-    github: "witch-factory", // GitHub username
-    twitter: "", // Twitter username
-    facebook: "", // Facebook username
-    linkedin: "", // LinkedIn username
-    instagram: "", // Instagram username
+    github: 'witch-factory', // GitHub username
+    twitter: '', // Twitter username
+    facebook: '', // Facebook username
+    linkedin: '', // LinkedIn username
+    instagram: '', // Instagram username
   },
   comment: {
-    utterances: "witch-factory/witch-blog", // Your GitHub repository
-    disqusShortName: "", // Disqus short name
+    utterances: 'witch-factory/witch-blog', // Your GitHub repository
+    disqusShortName: '', // Disqus short name
   },
-  thumbnail: "src/assets/images/thumbnail.png",
-  icon: "src/images/gatsby-icon.png", // path of blog icon
-  googleAnalytics: "G-HBQKJEYL1K", // Google Analytics tracking id
+  thumbnail: 'src/assets/images/thumbnail.png',
+  icon: 'src/images/gatsby-icon.png', // path of blog icon
+  googleAnalytics: 'G-HBQKJEYL1K', // Google Analytics tracking id
 };
 
 export default config;
